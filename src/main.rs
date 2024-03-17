fb3baa4e8f4f6387abe970c97ca4822cbed634b5
mykola2312 <nikolay_tihonov@i.ua>
2024-03-17 15:23:49 +0200

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(short='P', long, help="Path to directory you want to be filepwn'd")]
+    path: String,
+    
+    #[arg(short='u', long, help="name of user, as string")]
+    uid: String,
+
+    #[arg(short='g', long, help="name of group, as string")]
+    gid: u32,
+
+    #[arg(short='f', long, help="File permissions, in octal")]
+    file_permissions: String,
+
+    #[arg(short='d', long, help="Directory permissions, in octal")]
+    directory_permissions: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(args);
 }
